190ffc9602f461d273f81802a92dffbac38286b5
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-28 22:17:49 +0530

    addnews

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsAddComponent } from './news-add/news-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsAddComponent
   ],
   imports: [
     BrowserModule,
