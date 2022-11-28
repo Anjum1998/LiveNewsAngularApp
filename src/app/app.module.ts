a6b71ce275364411eb5d58604e557d57eab28010
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-28 22:40:52 +0530

    addview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsAddComponent } from './news-add/news-add.component';
+import { NewsViewComponent } from './news-view/news-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsAddComponent
+    NewsAddComponent,
+    NewsViewComponent
   ],
   imports: [
     BrowserModule,
